2d6eb695c935ca6374c70ef88a74c6ed7862837a
Ryan Hollins <ballbaroi@gmail.com>
2020-02-18 15:15:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,6 @@
-def reverse_each_word(array)
-  new_array = array.split (" ")
-  reversed_array = new_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
-end
-
-def reverse_each_word(sentence)
-  new_array = sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
+def reverse_each_word(string)
+  new_array = string.split
+  new_array.collect do |i|
+    newstr = "#{i.reverse}"
+  end
 end
