34441a90eb35bf2f0749bfb13f273f97e6fdf473
Ryan Hollins <ballbaroi@gmail.com>
2020-02-18 15:11:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,8 +4,8 @@ def reverse_each_word(array)
   return reversed_array.join(" ")
 end
 
-def reverse_each_word(array)
-  new_array = array.split(" ")
+def reverse_each_word(sentence)
+  new_array = sentence.split(" ")
   new_array = []
   new_array = words_array.collect {|word| word.reverse }
   new_array.join(" ")
