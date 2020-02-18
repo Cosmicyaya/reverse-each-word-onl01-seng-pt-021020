5ec0a304ed47e249902f48593eb86a43178cc32d
Ryan Hollins <ballbaroi@gmail.com>
2020-02-18 15:05:39 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,5 +2,5 @@ def reverse_each_word(array)
   new_array = array.split (" ")
   reversed_array = new_array.each {|x| x.reverse!}
   return reversed_array.join(" ")
-  puts reversed_array.collect
+  
 end
